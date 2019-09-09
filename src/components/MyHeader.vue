a6c5fa87d3a34de2f5192d3c46e69d833fc88f74
mayckol <mayckoll@live.com>
2019-09-09 08:14:53 -0300

    first commit

---
+++ src/components/MyHeader.vue
@@ -0,0 +1,56 @@
+<template>
+  <nav class="navbar navbar-expand-lg navbar-light bg-primary">
+    <slot></slot>
+  </nav>
+</template>
+
+<script>
+    export default {
+        name: "MyHeader"
+    }
+</script>
+
+<style scoped>
+
+  .navbar {
+    font-family: "Ubuntu Mono";
+  }
+  .navbar-light .navbar-nav .nav-link {
+    color: #ffffff;
+  }
+
+  .navbar-light a {
+    color: #ffffff;
+  }
+
+  .navbar li {
+    margin: 15px 40px 0 0;
+  }
+
+  .navbar-brand {
+    margin: 15px 100px 0 0;
+  }
+  .navbar-light .navbar-nav .nav-link a:hover {
+    color: black;
+  }
+  .dropdown-menu {
+    padding: 0;
+    min-width: 1rem;
+    max-width: 3rem;
+    background-color: #007bff;
+    border: 1px solid white;
+  }
+
+  .dropdown-item {
+    padding: .15rem .75rem;
+    font-size: 15px;
+    text-align: left;
+  }
+
+  .dropdown-menu a:hover {
+    background-color: transparent;
+    border: 1px solid white;
+  }
+
+
+</style>
---
+++ src/pages/Login.vue
@@ -0,0 +1,58 @@
+<template>
+  <div>
+    <my-header>
+      <slot>
+        <a class="navbar-brand" href="#">Home</a>
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav"
+                aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+          <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarNav">
+          <ul class="navbar-nav">
+            <li class="nav-item">
+              <a class="nav-link" href="#">PRO</a>
+            </li>
+            <li class="nav-item dropdown">
+              <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown"
+                 aria-haspopup="true" aria-expanded="false">
+                PT
+              </a>
+              <div class="dropdown-menu" aria-labelledby="navbarDropdown">
+                <a class="dropdown-item" href="#">EN</a>
+              </div>
+            </li>
+          </ul>
+        </div>
+      </slot>
+    </my-header>
+    <div class="container">
+      <div class="row">
+        <div class="col-sm" id="logo">
+          Uma de três colunas
+        </div>
+        <div class="col-sm">
+          Uma de três colunas
+        </div>
+        <div class="col-sm">
+          Uma de três colunas
+        </div>
+      </div>
+    </div>
+  </div>
+
+</template>
+
+<script>
+    import MyHeader from "../components/MyHeader"
+
+    export default {
+        name: "Login",
+        components: {MyHeader}
+    }
+</script>
+
+<style scoped>
+  #logo{
+    background-image: url("../../static/bet3.png");
+  }
+</style>
