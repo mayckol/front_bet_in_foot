55c88c0416bd84a591e095653a7320cc60275130
mayckol <mayckoll@live.com>
2019-10-13 23:47:02 -0300

    Adicionado view para ADM

--- src/components/Adm.vue
+++ src/components/Adm.vue
@@ -1,18 +1,61 @@
 <template>
-    <div>
-
+    <div id="app">
+        <slider :links="navLinks" :width="300" format="overlay" direction="left" :opacity="0.15"></slider>
+        <div class="container text-center">
+            <div class="row">
+                <div class="col mt-5">
+                    <h1>Seja bem vindo {{ user }}</h1>
+                    <p>Para acessar o painel de opções clique no botão ao lado esquerdo</p>
+                </div>
+            </div>
+            <div v-if="matchs" class="col">
+               <register-matches></register-matches>
+            </div>
+            <div v-if="registerPro" class="col">
+                pro
+            </div>
+        </div>
     </div>
 </template>
 
 <script>
+    import Slider from '@jeremyhamm/vue-slider'
+    import RegisterMatches from "@/components/RegisterMatches"
     export default {
-        name: "Adm"
+        name: "Adm",
+        data() {
+            return {
+                matchs: true,
+                registerPro: false,
+                navLinks: [
+                    {'id': 1, 'text': 'Cadastrar nova rodada', 'url': '#'},
+                    {'id': 2, 'text': 'Habilitar Usuario PRO', 'url': '#'}
+                ]
+            }
+        },
+        components: {
+            Slider,
+            RegisterMatches
+        },
+        computed: {
+            user() {
+                return this.$store.getters.userName
+            },
+        },
+        created() {
+            this.$store.dispatch('getUserData')
+        }
     }
 </script>
 
 <style>
     body {
         background-color: white;
+    }
+
+    .navMenu a {
+        color: white !important;
+        font-size: 20px !important;
     }
 
 </style>
---
+++ src/components/RegisterMatches.vue
@@ -0,0 +1,156 @@
+<template>
+    <form action="#" @submit.stop.prevent="registerMatches">
+        <div class="row text-center border-all p-3 row-hover">
+            <div class="col-2"><p>Jogo 1</p></div>
+            <div class="col-sm-4">
+                <select style="margin-bottom: 15px" v-model="sendingTeam1" class="form-control">
+                    <option v-for="(team, index) in this.$store.state.teams" :value="{ id: team.id, text: team.name }">
+                        {{ team.name }}
+                    </option>
+                </select>
+            </div>
+            <div class="col-sm-4">
+                <select style="margin-bottom: 15px" v-model="visitingTeam1" class="form-control">
+                    <option v-for="(team, index) in this.$store.state.teams" :value="{ id: team.id, text: team.name }">
+                        {{ team.name }}
+                    </option>
+                </select>
+            </div>
+            <div class="col-sm-12">
+                <h5>Descrição</h5>
+                <input type="text">
+            </div>
+        </div>
+        <div class="row text-center border-all p-3 row-hover">
+            <div class="col-2"><p>Jogo 2</p></div>
+            <div class="col-sm-4">
+                <select style="margin-bottom: 15px" v-model="sendingTeam2" class="form-control">
+                    <option v-for="(team, index) in this.$store.state.teams" :value="{ id: team.id, text: team.name }">
+                        {{ team.name }}
+                    </option>
+                </select>
+            </div>
+            <div class="col-sm-4">
+                <select style="margin-bottom: 15px" v-model="visitingTeam2" class="form-control">
+                    <option v-for="(team, index) in this.$store.state.teams" :value="{ id: team.id, text: team.name }">
+                        {{ team.name }}
+                    </option>
+                </select>
+            </div>
+            <div class="col-sm-12">
+                <h5>Descrição</h5>
+                <input type="text">
+            </div>
+        </div>
+        <div class="row text-center border-all p-3 row-hover">
+            <div class="col-2"><p>Jogo 3</p></div>
+            <div class="col-sm-4">
+                <select style="margin-bottom: 15px" v-model="sendingTeam3" class="form-control">
+                    <option v-for="(team, index) in this.$store.state.teams" :value="{ id: team.id, text: team.name }">
+                        {{ team.name }}
+                    </option>
+                </select>
+            </div>
+            <div class="col-sm-4">
+                <select style="margin-bottom: 15px" v-model="visitingTeam3" class="form-control">
+                    <option v-for="(team, index) in this.$store.state.teams" :value="{ id: team.id, text: team.name }">
+                        {{ team.name }}
+                    </option>
+                </select>
+            </div>
+            <div class="col-sm-12">
+                <h5>Descrição</h5>
+                <input type="text">
+            </div>
+        </div>
+        <div class="row text-center border-all p-3 row-hover">
+            <div class="col-2"><p>Jogo 4</p></div>
+            <div class="col-sm-4">
+                <select style="margin-bottom: 15px" v-model="sendingTeam4" class="form-control">
+                    <option v-for="(team, index) in this.$store.state.teams" :value="{ id: team.id, text: team.name }">
+                        {{ team.name }}
+                    </option>
+                </select>
+            </div>
+            <div class="col-sm-4">
+                <select style="margin-bottom: 15px" v-model="visitingTeam4" class="form-control">
+                    <option v-for="(team, index) in this.$store.state.teams" :value="{ id: team.id, text: team.name }">
+                        {{ team.name }}
+                    </option>
+                </select>
+            </div>
+            <div class="col-sm-12">
+                <h5>Descrição</h5>
+                <input type="text">
+            </div>
+        </div>
+        <div class="row text-center border-all p-3 row-hover">
+            <div class="col-2"><p>Jogo 5</p></div>
+            <div class="col-sm-4">
+                <select style="margin-bottom: 15px" v-model="visitingTeam5" class="form-control">
+                    <option v-for="(team, index) in this.$store.state.teams" :value="{ id: team.id, text: team.name }">
+                        {{ team.name }}
+                    </option>
+                </select>
+            </div>
+            <div class="col-sm-4">
+                <select style="margin-bottom: 15px" v-model="visitingTeam5" class="form-control">
+                    <option v-for="(team, index) in this.$store.state.teams" :value="{ id: team.id, text: team.name }">
+                        {{ team.name }}
+                    </option>
+                </select>
+            </div>
+            <div class="col-sm-12">
+                <h5>Descrição</h5>
+                <input type="text">
+            </div>
+        </div>
+        <div class="row pt-3">
+            <div class="col-sm-12 text-right">
+                <button class="btn btn-success" type="submit">Cadastrar</button>
+            </div>
+        </div>
+    </form>
+</template>
+
+<script>
+    export default {
+        name: "RegisterMatches",
+        data() {
+            return {
+                sendingTeam1: '',
+                sendingTeam2: '',
+                sendingTeam3: '',
+                sendingTeam4: '',
+                sendingTeam5: '',
+                visitingTeam1: '',
+                visitingTeam2: '',
+                visitingTeam3: '',
+                visitingTeam4: '',
+                visitingTeam5: '',
+            }
+        },
+        methods: {
+            registerMatches() {
+                console.log()
+            }
+        },
+        created() {
+            return this.$store.dispatch('getTeams')
+        }
+    }
+</script>
+
+<style scoped>
+
+    .border-all {
+        border: 1px solid black;
+        border-radius: 10px;
+    }
+
+    .row-hover:hover {
+        background-color: #3b889a;
+    }
+
+
+</style>
